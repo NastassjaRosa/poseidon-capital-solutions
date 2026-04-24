f1bf2bb4909e7934cca91ccca36666bdd5c3eb69
NastassjaPro <nastassja.paulhiac@mtb111.fr>
2026-04-24 07:56:23 +0200

    netoyage

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -3,7 +3,6 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
 @Entity
